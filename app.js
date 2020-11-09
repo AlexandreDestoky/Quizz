0d5549e5618517bb21b8cf624629962d2590f18b
Destoky <adestoky@gmail.com>
2020-11-09 17:06:29 +0100

    Comptage + affichage résultats
    Affichage de la note + texte après avoir compté le nombre d'erreurs commise

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 //Sélection
 const form = document.querySelector(".formulaire");
 const questions = document.querySelectorAll(".blockQuestion input");
-const titreResulat = document.querySelector(".resultats h2");
-const texteResultat = document.querySelector(".note");
+const titreResultat = document.querySelector(".resultats h2");
+const noteResultat = document.querySelector(".note");
 const aideResultat = document.querySelector(".aide");
 const toutesLesQuestions = document.querySelectorAll(".blockQuestion");
 //Données
@@ -11,6 +11,7 @@ const emojis = ["🧙", "✨", "👀", "😭", "👎"];
 let tabResultats = [];
 let verifTableau = [];
 
+//lorsque l'on clique sur le bouton
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   // console.log(document.querySelector('input[name="q1"]:checked').value); Autre façon
@@ -23,6 +24,7 @@ form.addEventListener("submit", (e) => {
   tabResultats = [];
 });
 
+//Vérifie les données envoyé avec les données correctes
 let verifFunc = (tabAverifier) => {
   for (let i = 0; i < toutesLesQuestions.length; i++) {
     if (tabAverifier[i] == bonneReponses[i]) {
@@ -31,6 +33,46 @@ let verifFunc = (tabAverifier) => {
       verifTableau.push(false);
     }
   }
-  console.log(verifTableau);
+  afficherResultats(verifTableau);
   verifTableau = [];
 };
+
+let afficherResultats = (tabCheck) => {
+  let nbrFautes = tabCheck.filter((el) => el == false).length;
+  switch (nbrFautes) {
+    case 0:
+      titreResultat.innerText = `🧙 Bravo, c'est un sans faute ! 🧙`;
+      aideResultat.innerText = "";
+      noteResultat.innerText = "5/5";
+      break;
+    case 1:
+      titreResultat.innerText = `✨ Vous y êtes presque ! ✨`;
+      aideResultat.innerText = "Retentez une autre réponse dans la case rouge, puis re-validez !";
+      noteResultat.innerText = "4/5";
+      break;
+    case 2:
+      titreResultat.innerText = `✨ Encore un effort ... 👀`;
+      aideResultat.innerText = "Retentez une autre réponse dans les cases rouges, puis re-validez !";
+      noteResultat.innerText = "3/5";
+      break;
+    case 3:
+      titreResultat.innerText = `👀 Il reste quelques erreurs. 😭`;
+      aideResultat.innerText = "Retentez une autre réponse dans les cases rouges, puis re-validez !";
+      noteResultat.innerText = "2/5";
+      break;
+    case 4:
+      titreResultat.innerText = `😭 Peux mieux faire ! 😭`;
+      aideResultat.innerText = "Retentez une autre réponse dans les cases rouges, puis re-validez !";
+      noteResultat.innerText = "1/5";
+      break;
+    case 5:
+      titreResultat.innerText = `👎 Peux mieux faire ! 👎`;
+      aideResultat.innerText = "Retentez une autre réponse dans les cases rouges, puis re-validez !";
+      noteResultat.innerText = "0/5";
+      break;
+  
+    default:
+      "Wops, cas innatendu.";
+  }
+};
+
