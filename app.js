4e32a77b5f27d97f6a4e05622426b9b28d734e9c
Destoky <adestoky@gmail.com>
2020-11-09 15:43:11 +0100

    Obtention des résultats
    comparaison resultats avec bonne reponses et stockage dans un tableau de si oui ou non la réponse est bonne

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
+//Sélection
 const form = document.querySelector(".formulaire");
 const questions = document.querySelectorAll(".blockQuestion input");
+const titreResulat = document.querySelector(".resultats h2");
+const texteResultat = document.querySelector(".note");
+const aideResultat = document.querySelector(".aide");
+const toutesLesQuestions = document.querySelectorAll(".blockQuestion");
+//Données
+const bonneReponses = ["b", "c", "b", "c", "a"];
+const emojis = ["🧙", "✨", "👀", "😭", "👎"];
 let tabResultats = [];
-
+let verifTableau = [];
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   // console.log(document.querySelector('input[name="q1"]:checked').value); Autre façon
   for (const question of questions) {
-    if(question.checked) {
+    if (question.checked) {
       tabResultats.push(question.value);
     }
   }
-  console.log(tabResultats);
+  verifFunc(tabResultats);
   tabResultats = [];
 });
+
+let verifFunc = (tabAverifier) => {
+  for (let i = 0; i < toutesLesQuestions.length; i++) {
+    if (tabAverifier[i] == bonneReponses[i]) {
+      verifTableau.push(true);
+    } else {
+      verifTableau.push(false);
+    }
+  }
+  console.log(verifTableau);
+  verifTableau = [];
+};
